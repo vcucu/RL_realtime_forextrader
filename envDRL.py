ce65cab33223e23629168c1f2e7eb7d13cd5518a
Veronika <veronika.cucorova@gmail.com>
2019-12-06 19:44:56 +0100

    environment done

--- envDRL.py
+++ envDRL.py
@@ -7,43 +7,62 @@ import itertools
 
 class TradingEnv(gym.Env):
 
-  def __init__(self, train_data, init_invest=20000):
+  def __init__(self, train_data, init_invest=10):
     # data
-    self.stock_price_history = np.around(train_data) # round up to integer to reduce state space
-    self.n_stock, self.n_step = self.stock_price_history.shape
+    self.stock_price_history = np.around(train_data, decimals = 4) # round up to integer to reduce state space
+    #self.n_stock, self.n_step = self.stock_price_history.shape
 
     # instance attributes
-    self.init_invest = init_invest
+    self.init_invest = init_invest #initial balance
     self.cur_step = None
     self.stock_owned = None
-    self.stock_price = None
-    self.cash_in_hand = None
+    self.balance = None #== position value
+    #self.nav = None #==balance + (b_c * position_val) if long and (a_c * position_val) if short
+    self.volume = None
+    self.bid_close = None
+    self.ask_close = None
+    self.bid_close_previous = None
+    self.ask_close_previous = None
+    self.volume_previous = None
 
     # action space
-    self.action_space = spaces.Discrete(3**self.n_stock)
+    #self.action_space = spaces.Discrete(3**self.n_stock)
 
     # observation space: give estimates in order to sample and build scaler
-    stock_max_price = self.stock_price_history.max(axis=1)
-    stock_range = [[0, init_invest * 2 // mx] for mx in stock_max_price]
-    price_range = [[0, mx] for mx in stock_max_price]
-    cash_in_hand_range = [[0, init_invest * 2]]
-    self.observation_space = spaces.MultiDiscrete(stock_range + price_range + cash_in_hand_range)
+    #stock_max_price = self.stock_price_history.max(axis=1)
+    #stock_range = [[0, init_invest * 2 // mx] for mx in stock_max_price]
+    #price_range = [[0, mx] for mx in stock_max_price]
+    #cash_in_hand_range = [[0, init_invest * 2]]
+    #self.observation_space = spaces.MultiDiscrete(stock_range + price_range + cash_in_hand_range)
 
     # seed and start
-    self._seed()
+    #self._seed()
     self._reset()
 
 
-  def _seed(self, seed=None):
-    self.np_random, seed = seeding.np_random(seed)
-    return [seed]
+  #def _seed(self, seed=None):
+    #self.np_random, seed = seeding.np_random(seed)
+    #return [seed]
 
 
   def _reset(self):
     self.cur_step = 0
-    self.stock_owned = [0] * self.n_stock
-    self.stock_price = self.stock_price_history[:, self.cur_step]
-    self.cash_in_hand = self.init_invest
+    self.stock_owned = 0 #If more forex pairs were traded: [0] * self.n_stock
+    self.init_invest = init_invest #initial balance
+    self.balance = init_invest
+    #update all prices and volume
+    self.bid_close = self.stock_price_history.iloc[cur_step]['b_c']
+    self.ask_close = self.stock_price_history.iloc[cur_step]['a_c']
+    self.volume = self.stock_price_history.iloc[cur_step]['volume']
+    if cur_step > 0 : #if there is an observation before this one
+        self.bid_close_previous = self.stock_price_history.iloc[cur_step-1]['b_c']
+        self.ask_close_previous = self.stock_price_history.iloc[cur_step-1]['a_c']
+        self.volume_previous = self.stock_price_history.iloc[cur_step-1]['volume']
+    else: #just pretend nothing changed since the last observation till this one
+        self.bid_close_previous = self.bid_close
+        self.ask_close_previous = self.ask_close
+        self.volume_previous = self.volume
+    #self.nav = None #==balance + (b_c * position_val) if long and (a_c * position_val) if short
     return self._get_obs()
 
 
@@ -51,52 +70,51 @@ class TradingEnv(gym.Env):
     assert self.action_space.contains(action)
     prev_val = self._get_val()
     self.cur_step += 1
-    self.stock_price = self.stock_price_history[:, self.cur_step] # update price
+    self.bid_close = self.stock_price_history.iloc[cur_step]['b_c']
+    self.ask_close = self.stock_price_history.iloc[cur_step]['a_c']
+    self.volume = self.stock_price_history.iloc[cur_step]['volume']
+    if cur_step > 0 : #if there is an observation before this one (robustness)
+        self.bid_close_previous = self.stock_price_history.iloc[cur_step-1]['b_c']
+        self.ask_close_previous = self.stock_price_history.iloc[cur_step-1]['a_c']
+        self.volume_previous = self.stock_price_history.iloc[cur_step-1]['volume']
+    else: #just pretend nothing changed since the last observation till this one
+        self.bid_close_previous = self.bid_close
+        self.ask_close_previous = self.ask_close
+        self.volume_previous = self.volume
     self._trade(action)
     cur_val = self._get_val()
     reward = cur_val - prev_val
-    done = self.cur_step == self.n_step - 1
+    done = (self.cur_step == len(self.stock_price_history - 1)
     info = {'cur_val': cur_val}
     return self._get_obs(), reward, done, info
 
 
   def _get_obs(self):
     obs = []
-    obs.extend(self.stock_owned)
-    obs.extend(list(self.stock_price))
-    obs.append(self.cash_in_hand)
+    obs.append(self.stock_owned) #use extend if there are multiple
+    obs.append(self.balance)
+    obs.append(self.bid_close)
+    obs.append(self.ask_close)
+    obs.append(self.volume)
+    obs.append(self.bid_close_previous)
+    obs.append(self.ask_close_previous)
+    obs.append(self.ask_close_previous)
     return obs
 
 
-  def _get_val(self):
-    return np.sum(self.stock_owned * self.stock_price) + self.cash_in_hand
+  def _get_val(self): #reward function
+    return (self.stock_owned * self.self.bid_close) + self.balance
 
 
   def _trade(self, action):
-    # all combo to sell(0), hold(1), or buy(2) stocks
-    action_combo = map(list, itertools.product([0, 1, 2], repeat=self.n_stock))
-    action_vec = action_combo[action]
 
-    # one pass to get sell/buy index
-    sell_index = []
-    buy_index = []
-    for i, a in enumerate(action_vec):
-      if a == 0:
-        sell_index.append(i)
-      elif a == 2:
-        buy_index.append(i)
-
-    # two passes: sell first, then buy; might be naive in real-world settings
-    if sell_index:
-      for i in sell_index:
-        self.cash_in_hand += self.stock_price[i] * self.stock_owned[i]
-        self.stock_owned[i] = 0
-    if buy_index:
-      can_buy = True
-      while can_buy:
-        for i in buy_index:
-          if self.cash_in_hand > self.stock_price[i]:
-            self.stock_owned[i] += 1 # buy one share
-            self.cash_in_hand -= self.stock_price[i]
-          else:
-            can_buy = False
+    if action == 0:
+        self.balance += self.bid_close * self.stock_owned
+        self.stock_owned = 0
+    elif action == 2:
+        can_buy = True #not sure how this is going to be used yet
+        if self.balance > self.ask_close * 100: #if we can buy at least 100 shares
+            self.stock_owned += 100
+            self.balance -= self.ask_close *100
+        else:
+            can_buy =False
--- trainDRL.py
+++ trainDRL.py
@@ -1,34 +1,40 @@
 if __name__ == "__main__":
     # initialize gym environment and the agent
-    env = gym.make('CartPole-v0')
-    agent = DQNAgent(env)
-    # Iterate the game
-    for e in range(episodes):
+    data = pd.read_csv("dataS15.csv")
+    train_data = data.iloc[:12900]#get 80% for training
+    test_data = data.iloc[12900:]#20% for validation
+    params = {"episodes_nr" : 100,
+              "initial_investment":10,
+              "train_or_test": "train"
+              "batch_size": 100}
+
+    env = TradingEnv(train_data)
+    agent = Agent(env)
+    if params['train_or_test'] == "test":
+        env = TradingEnv(test_data) #overwrite
+        agent.load(args.weights) #load previously calculate weights
+
+    for e in range(params['episodes_nr']):
         # reset state in the beginning of each game
         state = env.reset()
-        state = np.reshape(state, [1, 4])
-        # time_t represents each frame of the game
-        # Our goal is to keep the pole upright as long as possible until score of 500
-        # the more time_t the more score
-        for time_t in range(500):
-            # turn this on if you want to render
-            # env.render()
-            # Decide action
-            action = agent.act(state)
+        state = np.reshape(state, [1, 4]) #whyyy?
+
+        for tick in range(500): #for time in range(env.n_step): #lets make it at most 500 ticks =roughly 2 hours
+
+            action = agent.act(state) # Decide action
             # Advance the game to the next frame based on the action.
-            # Reward is 1 for every frame the pole survived
-            next_state, reward, done, _ = env.step(action)
-            next_state = np.reshape(next_state, [1, 4])
+            next_state, reward, done, info = env.step(action)
+            next_state = np.reshape(next_state, [1, 4]) #whyyy
+
             # Remember the previous state, action, reward, and done
             agent.remember(state, action, reward, next_state, done)
             # make next_state the new current state for the next frame.
             state = next_state
-            # done becomes True when the game ends
-            # ex) The agent drops the pole
+
             if done:
                 # print the score and break out of the loop
                 print("episode: {}/{}, score: {}"
-                      .format(e, episodes, time_t))
+                      .format(e, params['episodes_nr'], info['cur_val']))
                 break
         # train the agent with the experience of the episode
         agent.replay(32)
